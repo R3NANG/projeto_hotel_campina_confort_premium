50f79a9f3105c54efadd7bab844047937a3f0d84
Renan Gustavo <renangustavoo101@gmail.com>
2018-11-09 19:48:06 -0300

    Envio 10

--- dados.h
+++ dados.h
@@ -1,63 +1,59 @@
 FILE *p_contrato;
 
-void cabecalho();
-
 int dados() {
 	p_contrato = fopen("contratos.txt", "a");
 	if(p_contrato == NULL) {
-		printf("\nFalha na Criacao do Contrato!");
+		printf("\nFalha na Criacao do Contrato!\n");
 		system("pause");
 		return 0;
 	}
+	
+	struct dados {
+		char cpf[16];
+		char nome[100];
+		char endereco[150];
+		char tipodocartao[25];
+		char numerodocartao[25];
+		char validadedocartao[15];
+		char codigodeseguranca[10];
+		char dataInicial[15];
+	};
+	
+	struct dados recebimento;
+	
 	cabecalho();
-	printf("\nDigite o Nome Completo do Hospede:\n");
-	char nome[100];
+	printf("\nDigite o CPF do Hospede (ex: 952.268.050-80):\n");
 	setbuf(stdin, NULL);
-	fgets(nome, 100, stdin);
-	fprintf(p_contrato, "Nome: ");
-	fprintf(p_contrato, "%s", nome);
-	printf("Digite o CPF do Hospede (ex: 952.268.050-80):\n");
-	char cpf[16];
+	fgets(recebimento.cpf, 16, stdin);
+	fprintf(p_contrato, "CPF: %s", recebimento.cpf);
+	printf("Digite o Nome Completo do Hospede:\n");
 	setbuf(stdin, NULL);
-	fgets(cpf, 16, stdin);
-	fprintf(p_contrato, "CPF: ");
-	fprintf(p_contrato, "%s", cpf);
+	fgets(recebimento.nome, 100, stdin);
+	fprintf(p_contrato, "Nome: %s", recebimento.nome);
 	printf("Digite o Endereco de Residencia do Hospede:\n");
-	char ende[150];
 	setbuf(stdin, NULL);
-	fgets(ende, 150, stdin);
-	fprintf(p_contrato, "Endereco de Residencia: ");
-	fprintf(p_contrato, "%s", ende);
+	fgets(recebimento.endereco, 150, stdin);
+	fprintf(p_contrato, "Endereco de Residencia: %s", recebimento.endereco);
 	printf("Digite a Bandeira do Cartao de Credito do Hospede:\n");
-	char tipoc[25];
 	setbuf(stdin, NULL);
-	fgets(tipoc, 25, stdin);
-	fprintf(p_contrato, "Bandeira do Cartao de Credito: ");
-	fprintf(p_contrato, "%s", tipoc);
+	fgets(recebimento.tipodocartao, 25, stdin);
+	fprintf(p_contrato, "Bandeira do Cartao de Credito: %s", recebimento.tipodocartao);
 	printf("Digite o Numero do Cartao (CVV) do Hospede:\n");
-	char numeroc[25];
 	setbuf(stdin, NULL);
-	fgets(numeroc, 25, stdin);
-	fprintf(p_contrato, "Numero do Cartao (CVV): ");
-	fprintf(p_contrato, "%s", numeroc);
+	fgets(recebimento.numerodocartao, 25, stdin);
+	fprintf(p_contrato, "Numero do Cartao (CVV): %s", recebimento.numerodocartao);
 	printf("Digite a Validade do Cartao do Hospede (ex: 13/08/2019):\n");
-	char validade[15];
 	setbuf(stdin, NULL);
-	fgets(validade, 15, stdin);
-	fprintf(p_contrato, "Validade do Cartao: ");
-	fprintf(p_contrato, "%s", validade);
+	fgets(recebimento.validadedocartao, 15, stdin);
+	fprintf(p_contrato, "Validade do Cartao: %s", recebimento.validadedocartao);
 	printf("Digite o Codigo de Seguranca do Cartao do Hospede:\n");
-	char codigodes[10];
 	setbuf(stdin, NULL);
-	fgets(codigodes, 10, stdin);
-	fprintf(p_contrato, "Codigo de Seguranca do Cartao: ");
-	fprintf(p_contrato, "%s", codigodes);
+	fgets(recebimento.codigodeseguranca, 10, stdin);
+	fprintf(p_contrato, "Codigo de Seguranca do Cartao: %s", recebimento.codigodeseguranca);
 	printf("Digite a Data Inicial da Hospedagem do Hospede (ex: 13/03/2019):\n");
-	char dataInicial[15];
 	setbuf(stdin, NULL);
-	fgets(dataInicial, 15, stdin);
-	fprintf(p_contrato, "Data Inicial da Hospedagem: ");
-	fprintf(p_contrato, "%s", dataInicial);
+	fgets(recebimento.dataInicial, 15, stdin);
+	fprintf(p_contrato, "Data Inicial da Hospedagem: %s", recebimento.dataInicial);
 	fclose(p_contrato);
 	printf("Adicionado ao Contrato!\n");
 	system("pause");
--- main.c
+++ main.c
@@ -17,90 +17,100 @@ void cabecalho() {
 main() {
 	login();
 	int op1, altatemp, qntdenoites;
-	float soma1 = 0, soma2 = 0, soma3 = 0, somafinal = 0;
+	float soma1 = 0, soma2 = 0, soma3 = 0;
 	
-	do {
-		cabecalho();
-		printf("\n1 - Check-in.\n");
-		printf("2 - Check-out.\n");
-		printf("3 - Pesquisa de Contrato.\n");
-		printf("4 - Sair do Programa.\n");
-		printf("Qual opcao voce deseja?: ");
-		scanf("%d", &op1);
-	
-		switch(op1) {
-			case 1:
-				dados();
-				p_contrato = fopen("contratos.txt", "a");
-				if(p_contrato == NULL) {
-					printf("\nFalha na Criacao do Contrato!\n");
+	while(1) {
+		do {
+			cabecalho();
+			printf("\n1 - Check-in.\n");
+			printf("2 - Check-out.\n");
+			printf("3 - Pesquisa de Contrato.\n");
+			printf("4 - Sair do Programa.\n");
+			printf("Qual opcao voce deseja?: ");
+			scanf("%d", &op1);
+		
+			switch(op1) {
+				case 1:
+					dados();
+					p_contrato = fopen("contratos.txt", "a");
+					if(p_contrato == NULL) {
+						printf("\nFalha na Criacao do Contrato!\n");
+						system("pause");
+						return 0;
+					}
+					
+					do {
+						cabecalho();
+						printf("\nDigite a Quantidade de Noites que o Hospede ficara Hospedado:\n");
+						scanf("%d", &qntdenoites);
+						if(qntdenoites <= 0) {
+							printf("Quantidade Invalida.\n");
+							system("pause");
+						}
+						else {
+							fprintf(p_contrato, "Quantidade de Noites Hospedado: %d\n", qntdenoites);
+							printf("Adicionado ao Contrato!\n");
+							system("pause");
+						}
+					} while(qntdenoites <= 0);
+						
+					do {
+						cabecalho();
+						printf("\nO Hospede Deseja se Hospedar em Epoca de Alta Estacao? (ex: Natal, Reveillon, etc.)\n");
+						printf("1 - Sim / 2 - Nao: ");
+						scanf("%d", &altatemp);
+						switch(altatemp) {
+							case 1:
+								printf("Adicionado ao Contrato!\n");
+								system("pause");
+								fprintf(p_contrato, "Hospede Hospedado em Epoca de Alta Estacao.\n");
+								break;
+							case 2:
+								printf("Adicionado ao Contrato!\n");
+								system("pause");
+								fprintf(p_contrato, "Hospede Hospedado em Epoca Normal.\n");
+								break;
+							default:
+								printf("Opcao Invalida.\n");
+								system("pause");
+								break;
+						}
+					} while(altatemp < 1 || altatemp > 2);
+					
+					fclose(p_contrato);
+					soma1 = quartos(altatemp);
+					soma1 *= qntdenoites;
+					soma2 = babysitter(altatemp, qntdenoites);
+					soma3 = alugueldecarro(altatemp, qntdenoites);
+					
+					struct final {
+						float somafinal;
+					};
+					
+					struct final soma;
+					
+					soma.somafinal = soma1 + soma2 + soma3;
+					
+					p_contrato = fopen("contratos.txt", "a");
+					fprintf(p_contrato, "SOMA TOTAL: %.2f\n\n", soma.somafinal);
+					fclose(p_contrato);
+					break;
+				case 2:
+					printf("slaoq");
+					break;
+				case 3:
+					printf("slaoqtb");
+					break;
+				case 4:
+					printf("\nPrograma Encerrado.\n");
 					system("pause");
 					return 0;
-				}
-				
-				do {
-					cabecalho();
-					printf("\nDigite a Quantidade de Noites que o Hospede ficara Hospedado:\n");
-					scanf("%d", &qntdenoites);
-					if(qntdenoites <= 0) {
-						printf("Quantidade Invalida.\n");
-						system("pause");
-					}
-					else {
-						fprintf(p_contrato, "Quantidade de Noites Hospedado: %d\n", qntdenoites);
-						printf("Adicionado ao Contrato!\n");
-						system("pause");
-					}
-				} while(qntdenoites <= 0);
-					
-				do {
-					cabecalho();
-					printf("\nO Hospede Deseja se Hospedar em Epoca de Alta Estacao? (ex: Natal, Reveillon, etc.)\n");
-					printf("1 - Sim / 2 - Nao: ");
-					scanf("%d", &altatemp);
-					switch(altatemp) {
-						case 1:
-							printf("Adicionado ao Contrato!\n");
-							system("pause");
-							fprintf(p_contrato, "Hospede Hospedado em Epoca de Alta Estacao.\n");
-							break;
-						case 2:
-							printf("Adicionado ao Contrato!\n");
-							system("pause");
-							fprintf(p_contrato, "Hospede Hospedado em Epoca Normal.\n");
-							break;
-						default:
-							printf("Opcao Invalida.\n");
-							system("pause");
-							break;
-					}
-				} while(altatemp < 1 || altatemp > 2);
-				
-				fclose(p_contrato);
-				soma1 = quartos(altatemp);
-				soma1 *= qntdenoites;
-				soma2 = babysitter(altatemp, qntdenoites);
-				soma3 = alugueldecarro(altatemp, qntdenoites);
-				somafinal = soma1 + soma2 + soma3;
-				p_contrato = fopen("contratos.txt", "a");
-				fprintf(p_contrato, "SOMA TOTAL: %.2f\n\n", somafinal);
-				fclose(p_contrato);
-				break;
-			case 2:
-				printf("slaoq");
-				break;
-			case 3:
-				printf("slaoqtb");
-				break;
-			case 4:
-				printf("\nPrograma Encerrado.\n");
-				system("pause");
-				return 0;
-				break;
-			default:
-				printf("\nOpcao Invalida.\n");
-				system("pause");
-				break;
-		}
-	} while(op1 < 1 || op1 > 4);
+					break;
+				default:
+					printf("\nOpcao Invalida.\n");
+					system("pause");
+					break;
+			}
+		} while(op1 < 1 || op1 > 4);
+	}
 }
